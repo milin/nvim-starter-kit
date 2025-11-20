3e7e6d72f4193b5244d29f351f63a301d8d836bf
Milind Shakya <_milin@github.com>
2025-11-19 22:46:43 -0500

    Fix vimwiki for lua

--- .config/nvim/lua/plugins/vimwiki.lua
+++ .config/nvim/lua/plugins/vimwiki.lua
@@ -6,6 +6,42 @@ return {
     if vim.fn.exists("$TMUX") == 1 then
       return "VeryLazy"
     end
+    return nil
+  end,
+  config = function()
+    -- Define wiki configurations
+    local pro_wiki = {
+      path = '~/Box Sync/VimWiki/professional_development.wiki',
+      syntax = 'markdown',
+      ext = '.md'
+    }
+    
+    local per_wiki = {
+      path = '~/Box Sync/VimWiki/personal_development.wiki',
+      syntax = 'markdown',
+      ext = '.md'
+    }
+    
+    local shared_wiki = {
+      path = '~/Box Sync/VimWiki/shared.wiki',
+      syntax = 'markdown',
+      ext = '.md'
+    }
+    
+    -- Set vimwiki global variables
+    vim.g.vimwiki_listsyms = '✗○◐●✓'
+    vim.g.vimwiki_list = { pro_wiki, per_wiki, shared_wiki }
+    vim.g.vimwiki_folding = 'expr'
+    vim.g.vimwiki_table_mappings = 0
+    
+    -- Optional: Set up search commands and keymaps
+    vim.api.nvim_create_user_command('Ngrep', function(opts)
+      vim.cmd('vimgrep "' .. opts.args .. '" ~/Box\\ Sync/VimWiki/**/*.md')
+    end, { nargs = 1 })
+    
+    -- Set up keymaps for vimwiki search
+    vim.keymap.set('n', '<leader>m', ':Ngrep ', { desc = 'Search VimWiki' })
+    vim.keymap.set('n', '<leader>]', ':cnext<CR>', { silent = true, desc = 'Next search result' })
+    vim.keymap.set('n', '<leader>[', ':cprevious<CR>', { silent = true, desc = 'Previous search result' })
   end,
 }
-
