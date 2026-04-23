3401782c5c95602e4d107e500c9d8c118a14858b
Milind Shakya <milind.shakya@kraken.tech>
2026-04-23 11:27:55 -0400

    Revert "Removing harpoon for now since its causing some issues. will need to revert this later to get it working."
    This reverts commit 970b11251afec8fbfcfd6f9155b86d9c74d51fe3.

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -85,17 +85,17 @@ keymap.set("n", "<leader>gb", ":GitBlameToggle<CR>") -- toggle git blame
 keymap.set("n", "<leader>gg", ":LazyGit<CR>") 
 
 -- Harpoon
--- keymap.set("n", "<leader>ha", require("harpoon.mark").add_file)
--- keymap.set("n", "<leader>hh", require("harpoon.ui").toggle_quick_menu)
--- keymap.set("n", "<leader>h1", function() require("harpoon.ui").nav_file(1) end)
--- keymap.set("n", "<leader>h2", function() require("harpoon.ui").nav_file(2) end)
--- keymap.set("n", "<leader>h3", function() require("harpoon.ui").nav_file(3) end)
--- keymap.set("n", "<leader>h4", function() require("harpoon.ui").nav_file(4) end)
--- keymap.set("n", "<leader>h5", function() require("harpoon.ui").nav_file(5) end)
--- keymap.set("n", "<leader>h6", function() require("harpoon.ui").nav_file(6) end)
--- keymap.set("n", "<leader>h7", function() require("harpoon.ui").nav_file(7) end)
--- keymap.set("n", "<leader>h8", function() require("harpoon.ui").nav_file(8) end)
--- keymap.set("n", "<leader>h9", function() require("harpoon.ui").nav_file(9) end)
+keymap.set("n", "<leader>ha", require("harpoon.mark").add_file)
+keymap.set("n", "<leader>hh", require("harpoon.ui").toggle_quick_menu)
+keymap.set("n", "<leader>h1", function() require("harpoon.ui").nav_file(1) end)
+keymap.set("n", "<leader>h2", function() require("harpoon.ui").nav_file(2) end)
+keymap.set("n", "<leader>h3", function() require("harpoon.ui").nav_file(3) end)
+keymap.set("n", "<leader>h4", function() require("harpoon.ui").nav_file(4) end)
+keymap.set("n", "<leader>h5", function() require("harpoon.ui").nav_file(5) end)
+keymap.set("n", "<leader>h6", function() require("harpoon.ui").nav_file(6) end)
+keymap.set("n", "<leader>h7", function() require("harpoon.ui").nav_file(7) end)
+keymap.set("n", "<leader>h8", function() require("harpoon.ui").nav_file(8) end)
+keymap.set("n", "<leader>h9", function() require("harpoon.ui").nav_file(9) end)
 
 -- Vim REST Console
 keymap.set("n", "<leader>fr", ":call VrcQuery()<CR>") -- Run REST query
