fa241ae8226e6930f3251227e77bc04369f51028
Milind Shakya <milind.shakya@kraken.tech>
2026-05-01 16:36:11 -0400

    add integration test mode in vim-test and additional db source

--- .config/nvim/lua/plugins/nvim-dbui.lua
+++ .config/nvim/lua/plugins/nvim-dbui.lua
@@ -14,14 +14,20 @@ return {
     init = function()
       vim.g.db_ui_use_nerd_fonts = 1
       vim.g.db_ui_save_location = "~/development/dbui/"
+      -- If this is set to 1, opening any of the table helpers will also automatically execute the query.
+      vim.g.db_ui_auto_execute_table_helpers = 1
 
       vim.g.dbs = {
         {
-          name = 'test',
+          name = 'prod-sje',
+          url = 'postgres://127.0.0.1:5430/saintjohnenergy-prod.krakencore?sslmode=disable'
+        },
+        {
+          name = 'test-sje',
           url = 'postgres://127.0.0.1:5430/saintjohnenergy-test.krakencore?sslmode=disable'
         },
         {
-          name = 'local',
+          name = 'local-sje',
           url = 'postgres://127.0.0.1:5432/saintjohnenergy-consumer?sslmode=disable'
         },
       }
--- .config/nvim/lua/plugins/vim-test.lua
+++ .config/nvim/lua/plugins/vim-test.lua
@@ -7,6 +7,7 @@ return {
     { "<leader>rta", "<cmd>TestSuite<cr>", desc = "Run test suite" },
     { "<leader>rtl", "<cmd>TestLast<cr>", desc = "Run last test" },
     { "<leader>rtd", "<cmd>call ToggleTestDebug()<cr>", desc = "Toggle test debug mode" },
+    { "<leader>rti", "<cmd>call ToggleIntegrationTestMode()<cr>", desc = "Toggle Integration Test mode" },
   },
   config = function()
     vim.g["test#python#runner"] = "pytest"
@@ -14,6 +15,7 @@ return {
     vim.g["test#strategy"] = "toggleterm"
     vim.cmd([[
       let g:test_debug_mode = 0
+      let g:test_integration_mode = 0
 
       function! KrakenTransform(cmd)
         let l:root = '/Users/milind.shakya/development/kraken-core/src'
@@ -21,6 +23,8 @@ return {
         let l:args = substitute(l:args, ' src/', ' ', '')
         if g:test_debug_mode
           return 'cd ' . l:root . ' && python -m debugpy --listen 54546 --wait-for-client -m pytest ' . l:args
+        elseif g:test_integration_mode
+          return 'cd ' . l:root . ' && python -m pytest --create-db ' . l:args
         else
           return 'cd ' . l:root . ' && python -m pytest ' . l:args
         endif
@@ -30,6 +34,11 @@ return {
         let g:test_debug_mode = !g:test_debug_mode
         echo g:test_debug_mode ? "Test debug ON (port 54546)" : "Test debug OFF"
       endfunction
+      function! ToggleIntegrationTestMode()
+        let g:test_integration_mode = !g:test_integration_mode
+        echo g:test_integration_mode ? "Test Integration Test Mode on (--create-db)" : "Test integration mode OFF"
+      endfunction
+
 
       let g:test#custom_transformations = {'kraken': function('KrakenTransform')}
       let g:test#transformation = 'kraken'
